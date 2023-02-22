3d44b89e68c581baa1f7aac52d5a8f0d68e6bff0
deepak1175 <74853754+deepak1175@users.noreply.github.com>
2023-02-22 18:52:43 +0530

    Add files via upload

---
+++ client/src/components/admin/partials/AdminFooter.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import moment from "moment";
+
+const AdminFooter = (props) => {
+  return (
+    <Fragment>
+    </Fragment>
+  );
+};
+
+export default AdminFooter;
---
+++ client/src/components/shop/partials/Footer.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import moment from "moment";
+
+const Footer = (props) => {
+  return (
+    <Fragment>
+    </Fragment>
+  );
+};
+
+export default Footer;
